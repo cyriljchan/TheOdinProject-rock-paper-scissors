516bb5daefd47fedab3d7f5124cf4f7c85f54984
Cyril Jay L. Chan <cyriljchan@yahoo.com>
2023-06-11 21:45:09 +0800

    added win-lose-tie conditions

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ function getPlayerChoice() {
     let choice = prompt("Please type rock, paper, or scissors");
     return (choice[0].toUpperCase() + choice.slice(1).toLowerCase());
 }
+
+function playRound(playerSelection, computerSelection) {
+    let result = (playerSelection === computerSelection) ? "Tie" :
+    ((playerSelection === "Rock" && computerSelection === "Scissors") || 
+    (playerSelection === "Paper" && computerSelection === "Rock") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper")) ? "Win" : "Lose";
+
+    return result;
+}
